ba6772486e7e6819fff81b5fc42ecae1da9749d9
Eugene Fedorenko <eugene.fedor@gmail.com>
2025-09-18 12:10:20 -0700

    fix(rpm-deps): unify QEMU package references in rpm-deps.sh
    Signed-off-by: Eugene Fedorenko <eugene.fedor@gmail.com>

--- hack/rpm-deps.sh
+++ hack/rpm-deps.sh
@@ -56,7 +56,7 @@ testimage_main="
   iputils
   nmap-ncat
   procps-ng
-  qemu-img-${QEMU_VERSION}
+  qemu-${QEMU_VERSION}
   sevctl
   tar
   targetcli
@@ -90,27 +90,20 @@ launcherbase_main="
   libvirt-client-${LIBVIRT_VERSION}
   libvirt-daemon-driver-qemu-${LIBVIRT_VERSION}
   passt-${PASST_VERSION}
-  qemu-kvm-core-${QEMU_VERSION}
-  qemu-kvm-device-usb-host-${QEMU_VERSION}
+  qemu-${QEMU_VERSION}
   swtpm-tools-${SWTPM_VERSION}
 "
 launcherbase_x86_64="
   edk2-ovmf-${EDK2_VERSION}
-  qemu-kvm-device-display-virtio-gpu-${QEMU_VERSION}
-  qemu-kvm-device-display-virtio-vga-${QEMU_VERSION}
-  qemu-kvm-device-display-virtio-gpu-pci-${QEMU_VERSION}
-  qemu-kvm-device-usb-redirect-${QEMU_VERSION}
+  qemu-${QEMU_VERSION}
   seabios-${SEABIOS_VERSION}
 "
 launcherbase_aarch64="
   edk2-aarch64-${EDK2_VERSION}
-  qemu-kvm-device-usb-redirect-${QEMU_VERSION}
-  qemu-kvm-device-display-virtio-gpu-${QEMU_VERSION}
-  qemu-kvm-device-display-virtio-gpu-pci-${QEMU_VERSION}
+  qemu-${QEMU_VERSION}
 "
 launcherbase_s390x="
-  qemu-kvm-device-display-virtio-gpu-${QEMU_VERSION}
-  qemu-kvm-device-display-virtio-gpu-ccw-${QEMU_VERSION}
+  qemu-${QEMU_VERSION}
 "
 launcherbase_extra="
   ethtool
@@ -126,7 +119,7 @@ launcherbase_extra="
 "
 
 handlerbase_main="
-  qemu-img-${QEMU_VERSION}
+  qemu-${QEMU_VERSION}
   passt-${PASST_VERSION}
 "
 handlerbase_extra="
@@ -145,7 +138,7 @@ libguestfstools_main="
   libguestfs-${LIBGUESTFS_VERSION}
   guestfs-tools-${GUESTFSTOOLS_VERSION}
   libvirt-daemon-driver-qemu-${LIBVIRT_VERSION}
-  qemu-kvm-core-${QEMU_VERSION}
+  qemu-${QEMU_VERSION}
 "
 libguestfstools_x86_64="
   edk2-ovmf-${EDK2_VERSION}
