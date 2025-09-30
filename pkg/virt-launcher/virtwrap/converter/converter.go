0d07871909feca5207cd2bab9242dff5878b574b
Harshit Gupta <harshitgupta1337@gmail.com>
2025-09-30 09:53:47 -0400

    Use correct hypervisor device in handler and launcher (#49)
    Signed-off-by: Harshit Gupta <guptaharshit@microsoft.com>
    Co-authored-by: Harshit Gupta <guptaharshit@microsoft.com>

--- pkg/virt-launcher/virtwrap/converter/converter.go
+++ pkg/virt-launcher/virtwrap/converter/converter.go
@@ -1351,6 +1351,24 @@ func Convert_v1_VirtualMachineInstance_To_api_Domain(vmi *v1.VirtualMachineInsta
 		domainVCPUTopologyForHotplug(vmi, domain)
 	}
 
+	hypervisorPath := "/dev/kvm"
+	if c.Hypervisor != nil {
+		hypervisorPath = fmt.Sprintf("/dev/%s", c.Hypervisor.GetDevice())
+	}
+	if _, err := os.Stat(hypervisorPath); errors.Is(err, os.ErrNotExist) {
+		if c.AllowEmulation {
+			logger := log.DefaultLogger()
+			logger.Infof("Hardware emulation device '%s' not present. Using software emulation.", hypervisorPath)
+			domain.Spec.Type = "qemu"
+		} else {
+			return fmt.Errorf("hardware emulation device '%s' not present", hypervisorPath)
+		}
+	} else if err != nil {
+		return err
+	}
+
+	domain.Spec.SysInfo = &api.SysInfo{}
+
 	err = Convert_v1_Firmware_To_related_apis(vmi, domain, c)
 	if err != nil {
 		return err
