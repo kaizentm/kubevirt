8a70038bab2ae7afaa79dfe909b779616b2d2db8
Harshit Gupta <guptaharshit@microsoft.com>
2025-12-15 11:28:32 -0800

    Move common functions to util.go
    Signed-off-by: Harshit Gupta <guptaharshit@microsoft.com>

--- pkg/virt-handler/virt-runtime/kvm-runtime.go
+++ pkg/virt-handler/virt-runtime/kvm-runtime.go
@@ -1,7 +1,6 @@
 package virtruntime
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -9,7 +8,6 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
-	"unsafe"
 
 	"k8s.io/apimachinery/pkg/api/resource"
 	"kubevirt.io/client-go/log"
@@ -26,25 +24,9 @@ import (
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/api"
 )
 
-type maskType bool
-
-type cpuMask struct {
-	mask map[string]maskType
-}
-
-const (
-	enabled  maskType = true
-	disabled maskType = false
-)
-
 var (
-	// parse CPU Mask expressions
-	cpuRangeRegex  = regexp.MustCompile(`^(\d+)-(\d+)$`)
-	negateCPURegex = regexp.MustCompile(`^\^(\d+)$`)
-	singleCPURegex = regexp.MustCompile(`^(\d+)$`)
-
 	// parse thread comm value expression
-	vcpuRegex = regexp.MustCompile(`^CPU (\d+)/KVM\n$`) // These threads follow this naming pattern as their command value (/proc/{pid}/task/{taskid}/comm)
+	kvmVcpuRegex = regexp.MustCompile(`^CPU (\d+)/KVM\n$`) // These threads follow this naming pattern as their command value (/proc/{pid}/task/{taskid}/comm)
 	// QEMU uses threads to represent vCPUs.
 
 )
@@ -190,27 +172,6 @@ func (k *KvmVirtRuntime) adjustQemuProcessMemoryLimits(podIsoDetector isolation.
 	return nil
 }
 
-// setProcessMemoryLockRLimit Adjusts process MEMLOCK
-// soft-limit (current) and hard-limit (max) to the given size.
-func setProcessMemoryLockRLimit(pid int, size int64) error {
-	// standard golang libraries don't provide API to set runtime limits
-	// for other processes, so we have to directly call to kernel
-	rlimit := unix.Rlimit{
-		Cur: uint64(size),
-		Max: uint64(size),
-	}
-	_, _, errno := unix.RawSyscall6(unix.SYS_PRLIMIT64,
-		uintptr(pid),
-		uintptr(unix.RLIMIT_MEMLOCK),
-		uintptr(unsafe.Pointer(&rlimit)), // #nosec used in unix RawSyscall6
-		0, 0, 0)
-	if errno != 0 {
-		return fmt.Errorf("error setting prlimit: %v", errno)
-	}
-
-	return nil
-}
-
 func (k *KvmVirtRuntime) AdjustResources(podIsoDetector isolation.PodIsolationDetector, vmi *v1.VirtualMachineInstance, config *v1.KubeVirtConfiguration) error {
 	err := k.adjustQemuProcessMemoryLimits(podIsoDetector, vmi, config.AdditionalGuestMemoryOverheadRatio)
 	if err != nil {
@@ -331,37 +292,6 @@ func (k *KvmVirtRuntime) KvmPitPid(vmi *v1.VirtualMachineInstance) (int, error) 
 	return -1, nil
 }
 
-// TODO L1VH: move such functions to util
-// Returns the pid of "vmpid" as seen from the first pid namespace the task
-// belongs to.
-func GetNspid(vmpid int) (int, error) {
-	fpath := filepath.Join("proc", strconv.Itoa(vmpid), "status")
-	file, err := os.Open(fpath)
-	if err != nil {
-		return 0, err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if len(line) < 6 {
-			continue
-		}
-		if line[0:6] != "NSpid:" {
-			continue
-		}
-		s := strings.Fields(line)
-		if len(s) < 2 {
-			continue
-		}
-		val, err := strconv.Atoi(s[2])
-		return val, err
-	}
-
-	return -1, nil
-}
-
 func (k *KvmVirtRuntime) affinePitThread(vmi *v1.VirtualMachineInstance) error {
 	var Mask unix.CPUSet
 	Mask.Zero()
@@ -408,14 +338,14 @@ func (k *KvmVirtRuntime) affinePitThread(vmi *v1.VirtualMachineInstance) error {
 }
 
 func isVCPU(comm []byte) (string, bool) {
-	if !vcpuRegex.MatchString(string(comm)) {
+	if !kvmVcpuRegex.MatchString(string(comm)) {
 		return "", false
 	}
-	v := vcpuRegex.FindSubmatch(comm)
+	v := kvmVcpuRegex.FindSubmatch(comm)
 	return string(v[1]), true
 }
-func getVCPUThreadIDs(pid int) (map[string]string, error) {
 
+func getVCPUThreadIDs(pid int) (map[string]string, error) {
 	p := filepath.Join(string(os.PathSeparator), "proc", strconv.Itoa(pid), "task")
 	d, err := os.ReadDir(p)
 	if err != nil {
@@ -434,94 +364,4 @@ func getVCPUThreadIDs(pid int) (map[string]string, error) {
 		}
 	}
 	return ret, nil
-}
-
-// parseCPUMask parses the mask and maps the results into a structure that contains which
-// CPUs are enabled or disabled for the scheduling and priority changes.
-// This implementation reimplements the libvirt parsing logic defined here:
-// https://github.com/libvirt/libvirt/blob/56de80cb793aa7aedc45572f8b6ec3fc32c99309/src/util/virbitmap.c#L382
-// except that in this case it uses a map[string]maskType instead of a bit array.
-func parseCPUMask(mask string) (*cpuMask, error) {
-
-	vcpus := cpuMask{}
-	if len(mask) == 0 {
-		return &vcpus, nil
-	}
-	vcpus.mask = make(map[string]maskType)
-
-	masks := strings.Split(mask, ",")
-	for _, i := range masks {
-		m := strings.TrimSpace(i)
-		switch {
-		case cpuRangeRegex.MatchString(m):
-			match := cpuRangeRegex.FindSubmatch([]byte(m))
-			startID, err := strconv.Atoi(string(match[1]))
-			if err != nil {
-				return nil, err
-			}
-			endID, err := strconv.Atoi(string(match[2]))
-			if err != nil {
-				return nil, err
-			}
-			if startID < 0 {
-				return nil, fmt.Errorf("invalid vcpu mask start index `%d`", startID)
-			}
-			if endID < 0 {
-				return nil, fmt.Errorf("invalid vcpu mask end index `%d`", endID)
-			}
-			if startID > endID {
-				return nil, fmt.Errorf("invalid mask range `%d-%d`", startID, endID)
-			}
-			for id := startID; id <= endID; id++ {
-				vid := strconv.Itoa(id)
-				if !vcpus.has(vid) {
-					vcpus.set(vid, enabled)
-				}
-			}
-		case singleCPURegex.MatchString(m):
-			match := singleCPURegex.FindSubmatch([]byte(m))
-			vid, err := strconv.Atoi(string(match[1]))
-			if err != nil {
-				return nil, err
-			}
-			if vid < 0 {
-				return nil, fmt.Errorf("invalid vcpu index `%d`", vid)
-			}
-			if !vcpus.has(string(match[1])) {
-				vcpus.set(string(match[1]), enabled)
-			}
-		case negateCPURegex.MatchString(m):
-			match := negateCPURegex.FindSubmatch([]byte(m))
-			vid, err := strconv.Atoi(string(match[1]))
-			if err != nil {
-				return nil, err
-			}
-			if vid < 0 {
-				return nil, fmt.Errorf("invalid vcpu index `%d`", vid)
-			}
-			vcpus.set(string(match[1]), disabled)
-		default:
-			return nil, fmt.Errorf("invalid mask value '%s' in '%s'", i, mask)
-		}
-	}
-	return &vcpus, nil
-}
-
-func (c cpuMask) isEnabled(vcpuID string) bool {
-	if len(c.mask) == 0 {
-		return true
-	}
-	if t, ok := c.mask[vcpuID]; ok {
-		return t == enabled
-	}
-	return false
-}
-
-func (c *cpuMask) has(vcpuID string) bool {
-	_, ok := c.mask[vcpuID]
-	return ok
-}
-
-func (c *cpuMask) set(vcpuID string, mtype maskType) {
-	c.mask[vcpuID] = mtype
 }
