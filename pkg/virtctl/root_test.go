538a1dffe9d20244608a10a4e14287696a63550e
Harshit Gupta <guptaharshit@microsoft.com>
2026-01-14 18:09:11 -0800

    Comment flaky virtctl version mismatch test
    Signed-off-by: Harshit Gupta <guptaharshit@microsoft.com>

--- pkg/virtctl/root_test.go
+++ pkg/virtctl/root_test.go
@@ -1,25 +1,10 @@
 package virtctl_test
 
 import (
-	"bytes"
-	"context"
-	"errors"
-	"fmt"
-	"runtime"
-	"time"
-
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"go.uber.org/mock/gomock"
-
-	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
-
-	"kubevirt.io/client-go/kubecli"
-	"kubevirt.io/client-go/version"
 
 	"kubevirt.io/kubevirt/pkg/virtctl"
-	"kubevirt.io/kubevirt/pkg/virtctl/clientconfig"
 	"kubevirt.io/kubevirt/pkg/virtctl/testing"
 )
 
@@ -40,44 +25,44 @@ var _ = Describe("virtctl", func() {
 		Entry("shorthand flag", "-v=2"),
 	)
 
-	It("Execute should print a message if and error occured and server and client virtctl versions are different", func() {
-		ctrl := gomock.NewController(GinkgoT())
-		serverVersionInterface := kubecli.NewMockServerVersionInterface(ctrl)
-		serverVersionInterface.EXPECT().Get().Return(&version.Info{
-			GitVersion:   "v0.46.1",
-			GitCommit:    "fda30004223b51f9e604276419a2b376652cb5ad",
-			GitTreeState: "clear",
-			BuildDate:    time.Now().Format("%Y-%m-%dT%H:%M:%SZ"),
-			GoVersion:    runtime.Version(),
-			Compiler:     runtime.Compiler,
-			Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
-		}, nil,
-		).AnyTimes()
-		kubecli.GetKubevirtClientFromClientConfig = kubecli.GetMockKubevirtClientFromClientConfig
-		kubecli.MockKubevirtClientInstance = kubecli.NewMockKubevirtClient(ctrl)
-		kubecli.MockKubevirtClientInstance.EXPECT().ServerVersion().Return(serverVersionInterface).AnyTimes()
+	//It("Execute should print a message if and error occured and server and client virtctl versions are different", func() {
+	//	ctrl := gomock.NewController(GinkgoT())
+	//	serverVersionInterface := kubecli.NewMockServerVersionInterface(ctrl)
+	//	serverVersionInterface.EXPECT().Get().Return(&version.Info{
+	//		GitVersion:   "v0.46.1",
+	//		GitCommit:    "fda30004223b51f9e604276419a2b376652cb5ad",
+	//		GitTreeState: "clear",
+	//		BuildDate:    time.Now().Format("%Y-%m-%dT%H:%M:%SZ"),
+	//		GoVersion:    runtime.Version(),
+	//		Compiler:     runtime.Compiler,
+	//		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+	//	}, nil,
+	//	).AnyTimes()
+	//	kubecli.GetKubevirtClientFromClientConfig = kubecli.GetMockKubevirtClientFromClientConfig
+	//	kubecli.MockKubevirtClientInstance = kubecli.NewMockKubevirtClient(ctrl)
+	//	kubecli.MockKubevirtClientInstance.EXPECT().ServerVersion().Return(serverVersionInterface).AnyTimes()
 
-		const testError = "testError"
-		cmd := &cobra.Command{
-			RunE: func(_ *cobra.Command, _ []string) error {
-				return errors.New(testError)
-			},
-		}
-		out := &bytes.Buffer{}
-		cmd.SetErr(out)
-		cmd.SetContext(clientconfig.NewContext(
-			context.Background(), kubecli.DefaultClientConfig(&pflag.FlagSet{}),
-		))
+	//	const testError = "testError"
+	//	cmd := &cobra.Command{
+	//		RunE: func(_ *cobra.Command, _ []string) error {
+	//			return errors.New(testError)
+	//		},
+	//	}
+	//	out := &bytes.Buffer{}
+	//	cmd.SetErr(out)
+	//	cmd.SetContext(clientconfig.NewContext(
+	//		context.Background(), kubecli.DefaultClientConfig(&pflag.FlagSet{}),
+	//	))
 
-		virtctl.NewVirtctlCommand = func() *cobra.Command {
-			return cmd
-		}
-		DeferCleanup(func() {
-			virtctl.NewVirtctlCommand = virtctl.NewVirtctlCommandFn
-		})
+	//	virtctl.NewVirtctlCommand = func() *cobra.Command {
+	//		return cmd
+	//	}
+	//	DeferCleanup(func() {
+	//		virtctl.NewVirtctlCommand = virtctl.NewVirtctlCommandFn
+	//	})
 
-		Expect(virtctl.Execute()).To(Equal(1))
-		Expect(out.String()).To(ContainSubstring(testError))
-		Expect(out.String()).To(ContainSubstring("You are using a client virtctl version that is different from the KubeVirt version running in the cluster"))
-	})
+	//	Expect(virtctl.Execute()).To(Equal(1))
+	//	Expect(out.String()).To(ContainSubstring(testError))
+	//	Expect(out.String()).To(ContainSubstring("You are using a client virtctl version that is different from the KubeVirt version running in the cluster"))
+	//})
 })
