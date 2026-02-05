a72bfa11ca5bc4deeab0fe3e5a1da88b5665fbbe
Harshit Gupta <guptaharshit@microsoft.com>
2026-02-05 16:57:11 +0000

    Add KVM impl of LauncherHypervisorResources iface.
    Replace the KVM-specific logic invocation in virt-controller to invocn
    of the LauncherHypervisorResources iface. Update all references of old
    code in virt-controller to new interface-based code.
    
    Signed-off-by: Harshit Gupta <guptaharshit@microsoft.com>

--- pkg/virt-controller/services/template.go
+++ pkg/virt-controller/services/template.go
@@ -46,6 +46,7 @@ import (
 	"kubevirt.io/client-go/precond"
 
 	drautil "kubevirt.io/kubevirt/pkg/dra"
+	"kubevirt.io/kubevirt/pkg/hypervisor"
 	"kubevirt.io/kubevirt/pkg/pointer"
 
 	"kubevirt.io/kubevirt/pkg/apimachinery"
@@ -78,7 +79,6 @@ const (
 )
 
 const K8sDevicePrefix = "devices.kubevirt.io"
-const KvmDevice = K8sDevicePrefix + "/kvm"
 const TunDevice = K8sDevicePrefix + "/tun"
 const VhostNetDevice = K8sDevicePrefix + "/vhost-net"
 const SevDevice = K8sDevicePrefix + "/sev"
@@ -115,11 +115,6 @@ const EXT_LOG_VERBOSITY_THRESHOLD = 5
 const ephemeralStorageOverheadSize = "50M"
 
 const (
-	VirtLauncherMonitorOverhead = "25Mi"  // The `ps` RSS for virt-launcher-monitor
-	VirtLauncherOverhead        = "100Mi" // The `ps` RSS for the virt-launcher process
-	VirtlogdOverhead            = "25Mi"  // The `ps` RSS for virtlogd
-	VirtqemudOverhead           = "40Mi"  // The `ps` RSS for virtqemud
-	QemuOverhead                = "30Mi"  // The `ps` RSS for qemu, minus the RAM of its (stressed) guest, minus the virtual page table
 	// Default: limits.memory = 2*requests.memory
 	DefaultMemoryLimitOverheadRatio = float64(2.0)
 
@@ -910,9 +905,11 @@ func (t *TemplateService) newVolumeRenderer(vmi *v1.VirtualMachineInstance, imag
 
 func (t *TemplateService) newResourceRenderer(vmi *v1.VirtualMachineInstance, networkToResourceMap map[string]string) (*ResourceRenderer, error) {
 	vmiResources := vmi.Spec.Domain.Resources
+	hypervisorName := t.clusterConfig.GetHypervisor().Name
+	hypervisorResource := ConstructHypervisorResourceName(hypervisor.NewLauncherHypervisorResources(hypervisorName))
 	baseOptions := []ResourceRendererOption{
 		WithEphemeralStorageRequest(),
-		WithVirtualizationResources(getRequiredResources(vmi, t.clusterConfig.AllowEmulation())),
+		WithVirtualizationResources(getRequiredResources(vmi, hypervisorResource, t.clusterConfig.AllowEmulation())),
 	}
 
 	if err := validatePermittedHostDevices(&vmi.Spec, t.clusterConfig); err != nil {
@@ -921,6 +918,10 @@ func (t *TemplateService) newResourceRenderer(vmi *v1.VirtualMachineInstance, ne
 
 	options := append(baseOptions, t.VMIResourcePredicates(vmi, networkToResourceMap).Apply()...)
 	return NewResourceRenderer(vmiResources.Limits, vmiResources.Requests, options...), nil
+}
+
+func ConstructHypervisorResourceName(l hypervisor.LauncherHypervisorResources) k8sv1.ResourceName {
+	return k8sv1.ResourceName(K8sDevicePrefix + "/" + l.GetHypervisorDevice())
 }
 
 func sidecarVolumeMount(containerName string) k8sv1.VolumeMount {
@@ -1267,29 +1268,6 @@ func appendUniqueImagePullSecret(secrets []k8sv1.LocalObjectReference, newsecret
 	return append(secrets, newsecret)
 }
 
-func addProbeOverheads(vmi *v1.VirtualMachineInstance, quantity *resource.Quantity) {
-	// We need to add this overhead due to potential issues when using exec probes.
-	// In certain situations depending on things like node size and kernel versions
-	// the exec probe can cause a significant memory overhead that results in the pod getting OOM killed.
-	// To prevent this, we add this overhead until we have a better way of doing exec probes.
-	// The virtProbeTotalAdditionalOverhead is added for the virt-probe binary we use for probing and
-	// only added once, while the virtProbeOverhead is the general memory consumption of virt-probe
-	// that we add per added probe.
-	virtProbeTotalAdditionalOverhead := resource.MustParse("100Mi")
-	virtProbeOverhead := resource.MustParse("10Mi")
-	hasLiveness := vmi.Spec.LivenessProbe != nil && vmi.Spec.LivenessProbe.Exec != nil
-	hasReadiness := vmi.Spec.ReadinessProbe != nil && vmi.Spec.ReadinessProbe.Exec != nil
-	if hasLiveness {
-		quantity.Add(virtProbeOverhead)
-	}
-	if hasReadiness {
-		quantity.Add(virtProbeOverhead)
-	}
-	if hasLiveness || hasReadiness {
-		quantity.Add(virtProbeTotalAdditionalOverhead)
-	}
-}
-
 func HaveContainerDiskVolume(volumes []v1.Volume) bool {
 	for _, volume := range volumes {
 		if volume.ContainerDisk != nil {
@@ -1578,7 +1556,8 @@ func CalculateMemoryOverhead(clusterConfig *virtconfig.ClusterConfig, netBinding
 	if vmiCPUArch == "" {
 		vmiCPUArch = clusterConfig.GetClusterCPUArch()
 	}
-	memoryOverhead := GetMemoryOverhead(vmi, vmiCPUArch, clusterConfig.GetConfig().AdditionalGuestMemoryOverheadRatio)
+
+	memoryOverhead := hypervisor.NewLauncherHypervisorResources(clusterConfig.GetHypervisor().Name).GetMemoryOverhead(vmi, vmiCPUArch, clusterConfig.GetConfig().AdditionalGuestMemoryOverheadRatio)
 
 	if netBindingPluginMemoryCalculator != nil {
 		memoryOverhead.Add(
