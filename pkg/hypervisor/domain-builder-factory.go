8b7b79153d786a809491bc0cc8ff4533f082fdcd
Harshit Gupta <guptaharshit@microsoft.com>
2026-02-25 12:53:40 +0000

    Implement KVM's MakeDomainBuilder fn
    Move existing DomainBuilder construction logic in converter.go's
    Convert_v1_VirtualMachineInstance_To_api_Domain function to new KVM
    hypervisor's MakeDomainBuilder function. Invoke the KVM fn from
    converter.go.
    
    Signed-off-by: Harshit Gupta <guptaharshit@microsoft.com>

--- pkg/hypervisor/domain-builder-factory.go
+++ pkg/hypervisor/domain-builder-factory.go
@@ -3,9 +3,14 @@ package hypervisor
 import (
 	v1 "kubevirt.io/api/core/v1"
 
+	"kubevirt.io/kubevirt/pkg/hypervisor/kvm"
 	"kubevirt.io/kubevirt/pkg/virt-launcher/virtwrap/converter/types"
 )
 
 func MakeDomainBuilder(hypervisor string, vmi *v1.VirtualMachineInstance, c *types.ConverterContext) *types.DomainBuilder {
-	return nil
+	switch hypervisor {
+	// Other hypervisors can be added here
+	default:
+		return kvm.MakeDomainBuilder(vmi, c)
+	}
 }
